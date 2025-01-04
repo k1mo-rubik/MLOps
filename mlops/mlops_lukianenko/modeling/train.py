34efba852e460c96bcebeecc2ff753e020d0fc27
k1mo rub1k <vitaliy0422@gmail.com>
2025-01-04 20:13:24 +0300

    перенос кода тренировки модели в train.py

--- mlops/mlops_lukianenko/modeling/train.py
+++ mlops/mlops_lukianenko/modeling/train.py
@@ -1,30 +1,77 @@
-from pathlib import Path
-
-import typer
-from loguru import logger
-from tqdm import tqdm
-
-from mlops_lukianenko.config import MODELS_DIR, PROCESSED_DATA_DIR
-
-app = typer.Typer()
+import torch
 
 
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    features_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
-    model_path: Path = MODELS_DIR / "model.pkl",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Training some model...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Modeling training complete.")
-    # -----------------------------------------
+def freeze_params(pretrained_model):
+    for param in pretrained_model.parameters():
+        param.requires_grad = False
 
 
-if __name__ == "__main__":
-    app()
+def swap_head(pretrained_model, num_classes):
+    pretrained_model.fc = torch.nn.Sequential(
+        torch.nn.Linear(pretrained_model.fc.in_features, 512),
+        torch.nn.ReLU(),
+        torch.nn.Dropout(0.4),
+        torch.nn.Linear(512, num_classes)
+    )
+
+
+def train_freezed(pretrained_model, num_epochs, train_loader, test_loader, device, loss_fn, optimizer, scheduler):
+    train_mode_label = "Freezed training"
+    train_process(num_epochs, pretrained_model, train_mode_label, train_loader, test_loader, device, loss_fn, optimizer, scheduler)
+
+
+def unfreeze(pretrained_model):
+    # Разморозка всех параметров модели
+    for param in pretrained_model.parameters():
+        param.requires_grad = True
+
+
+def train_process(num_epochs, pretrained_model, train_mode_label, train_loader, test_loader, device, loss_fn, optimizer, scheduler):
+    for epoch in range(num_epochs):
+        print(f"{train_mode_label} Epoch {epoch + 1}/{num_epochs}")
+        pretrained_model.train()  # Устанавливаем режим тренировки
+        running_loss = 0.0
+
+        for inputs, labels in train_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            # Forward pass
+            outputs = pretrained_model(inputs)
+            loss = loss_fn(outputs, labels)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+        print(f"{train_mode_label} Training loss: {running_loss / len(train_loader)}")
+        # Оценка на тестовой выборке
+        test_metrix(pretrained_model,test_loader,device)
+
+        print(f"Fine-Tuning")
+        # Шаг изменения скорости обучения
+        scheduler.step()
+
+
+def train_fine_tune(num_epochs, pretrained_model, train_loader, test_loader, device, loss_fn, optimizer, scheduler):
+    pretrained_model.train()
+    train_mode_label = 'Fine-Tuning'
+
+    train_process(num_epochs, pretrained_model, train_mode_label, train_loader, test_loader, device, loss_fn, optimizer, scheduler)
+
+
+def test_metrix(pretrained_model, test_loader, device):
+    # Оценка на тестовой выборке
+    pretrained_model.eval()  # Устанавливаем режим оценки
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = pretrained_model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print(f"Test Accuracy: {100 * correct / total:.2f}%")
